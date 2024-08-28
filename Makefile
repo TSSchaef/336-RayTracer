179e66368f577d0d346af2bbe3207c5b90c174e4
TSSchaef <tschaeferstorm@gmail.com>
2024-08-28 09:01:06 -0500

    Output first image

--- Makefile
+++ Makefile
@@ -4,9 +4,17 @@ ray-tracer: main.o
 	gcc main.o -o ray-tracer 
 
 main.o: src/main.c
-	gcc -Wall -Werror -g src/main.c -c
+	gcc src/main.c -c
+
+#Second way to build the program with debugging enabled
+
+#ray-tracer-debug: main-debug.o
+#	gcc main-debug.o -o ray-tracer-debug
+
+#main-debug.o: src/main.c
+#	gcc -Wall -Werror -g src/main.c -c main-debug.o
 
 .PHONY: clean
 
 clean:
-	rm -f *.o core* ray-tracer valgrind*
+	rm -f *.o core* image.ppm ray-tracer *debug valgrind*
--- src/main.c
+++ src/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+    int image_width = 256;
+    int image_height = 256;
+
+    printf("P3\n %d %d\n255\n", image_width, image_height);
+
+    int i, j;
+    for(i = 0; i < image_height; i++) {
+        for(j = 0; j < image_width; j++){
+            double g = (i + 0.0) / (image_height - 1);
+            double r = (j + 0.0) / (image_width - 1);
+            double b = 0.0;
+
+            int ir = (int) 255.999 * r;
+            int ig = (int) 255.999 * g;
+            int ib = (int) 255.999 * b;
+
+            printf("%d %d %d\n", ir, ig, ib);
+        }
+    }
     return 0;
 }
